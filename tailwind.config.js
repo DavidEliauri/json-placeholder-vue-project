af419a5268499cd8de5f5456bccfff2ad9b33ac7
eliauri <georgy.eliauri@yandex.ru>
2023-08-20 08:36:46 +0300

    cleared init files & added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        container: {
+            padding: '1rem',
+            center: true
+        },
+        extend: {}
+    },
+    plugins: []
+};
